fe8e377d0d462c31b5eeefe2ebbca4edc3ad82c5
kristina25g <kristina.lff@gmail.com>
2018-12-23 15:42:48 +0100

    Dodat korisnicki interfejs za menadzera restorana, izmenjeni slucajevi uporebe u vezi sa zahtevima zaposlenih

---
+++ gui/menadzer-gui/myScripts.js
@@ -0,0 +1,9 @@
+<script type="text/javascript">
+    $(document).ready(function () {
+        $("izmeniBtn").click(
+        function (e) {
+                $("#eMail").prop('readonly', false);
+            }
+        });
+    });
+</script>
